e04cc808b0690d3a34d49191767710ce69191eb1
Michael Smitte <smittemichael@yahoo.com>
2023-02-16 19:09:32 -0500

    added logic to about and nav components to create navigation on page

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './nav.css'
+import { BiHomeAlt } from 'react-icons/bi'
+import { BiUserCircle } from 'react-icons/bi'
+import { BiBook } from 'react-icons/bi'
+import { BiMessageSquareDetail } from 'react-icons/bi'
+import {useState} from 'react'
 
-const nav = () => {
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUserCircle /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
-export default nav
+export default Nav
