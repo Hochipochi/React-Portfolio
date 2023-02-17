3ccbf3ac474f8fd62db852d8e1f01229387900d0
Michael Smitte <smittemichael@yahoo.com>
2023-02-17 16:20:33 -0500

    created contact section, added styling for contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineMarkEmailUnread } from 'react-icons/md'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineMarkEmailUnread className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>Smittemichael@yahoo.com</h5>
+            <a hrev='mailto:smittemichael@yahoo.com' target='_blank'>Send a Message</a>
+          </article>
+        </div>
+        {/* end of contact options */}
+        <form action=''>
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name="message" placeholder='Your Message Here' rows="5" required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+
+    </section>
   )
 }
 
