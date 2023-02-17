51603213e0a56105cce200a6e2a0ddbe40549f2e
Michael Smitte <smittemichael@yahoo.com>
2023-02-16 23:15:46 -0500

    created about section, added information to the about css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/duopic.jpg'
+import { FaAward } from 'react-icons/fa'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>My Fullstack Experience</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects / Portfolio</h5>
+              <small>List of my Accomplishments</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam quibusdam necessitatibus ducimus, perspiciatis similique cupiditate officiis vero magnam voluptate recusandae voluptatum illum esse omnis soluta culpa debitis? Fugit, eligendi iure!
+          </p>
+        </div>
+
+        <a href="#contact" className='btn btn-primary'>Contact Me</a>
+      </div>
+
+
+    </section>
   )
 }
 
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import Resume from '../../assets/M.Smitte Resume 2022.pdf'
 const CTA = () => {
   return (
       <div className='cta'>
-          <a href={Resume} download className='btn'>Download Resume</a>
+          <a href={Resume} download className='btn btn-primary'>Download Resume</a>
           <a href="#contact" className='btn btn-primary'>Contact Me!</a>
     </div>
   )
